152994694ebb6b12cf4b4aebc4bae536c1e84b14
Arthur Sandim de Oliveira <arthur.soliveira@inf.ufpel.edu.br>
2022-11-25 12:55:02 -0300

    Update MovimentaçãoTheta.ino

--- MovimentaçãoTheta/MovimentaçãoTheta.ino
+++ MovimentaçãoTheta/MovimentaçãoTheta.ino
@@ -1,27 +1,25 @@
-#define x 5
-#define y 6
-#define stoped 130
-#define max 200
-#define min 100
+#include <stdlib.h> // biblioteca para incluir as variáveis com definição de tamanho em bits
+
+#define potx A0 // Potenciômetro X
+#define poty A1 // Potenciômetro Y
+#define axisx 5 // Eixo X
+#define axisy 6 // Eixo Y
 
 void setup() {
+  pinMode(axisx, OUTPUT);
+  pinMode(axisy, OUTPUT);
+  pinMode(potx, INPUT);
+  pinMode(poty, INPUT);
+
   Serial.begin(9600);
-
-  pinMode(x,OUTPUT);
-  pinMode(y,OUTPUT);
-  pinMode(A0,INPUT);
-  pinMode(A1,INPUT);
-
 }
 
 void loop() {
-  int xr=analogRead(A0);
-  int yr=analogRead(A1);
+  uint8_t x = map(analogRead(potx), 0, 1023, 0, 255); // Map faz uma regra de 3 para converter o valor do potenciômetro para o valor do PWM
+  uint8_t y = map(analogRead(poty), 0, 1023, 0, 255);
+ 
+  analogWrite(axisx, x); // Escreve o valor na porta
+  analogWrite(axisy, y);
 
-  Serial.print(xr);
-  Serial.print(",");
-  Serial.println(yr);
-
-  analogWrite(A0,xr);
-  analogWrite(A1,yr);
+  Serial.print("%d, %d\n", x, y);//printa para o monitor serial    
 }
